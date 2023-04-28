5ef5c1e4432922d00d7528a22d6beaec00a7b832
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-04-28 11:20:22 +0300

    1

---
+++ src/Task_1_2/Task_1_2.java
@@ -0,0 +1,10 @@
+package Task_1_2;
+
+public class Task_1_2 {
+
+	public static void main(String[] args) {
+		// TODO Автоматически созданная заглушка метода
+
+	}
+
+}
---
+++ src/Task_1_3/Task_1_3.java
@@ -0,0 +1,10 @@
+package Task_1_3;
+
+public class Task_1_3 {
+
+	public static void main(String[] args) {
+		// TODO Автоматически созданная заглушка метода
+
+	}
+
+}
