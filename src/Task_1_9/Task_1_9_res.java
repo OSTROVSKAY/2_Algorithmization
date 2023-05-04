d483a9063f530954f0ca495f31b4cecc7b45e2f3
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-04 22:30:23 +0300

    1

--- src/Task_1_9/Task_1_9_res.java
+++ src/Task_1_9/Task_1_9_res.java
@@ -11,9 +11,10 @@ public class Task_1_9_res {
 		  
 	      int check = 1;
 		  int x = 0;
+		  
+		  Scanner scanner = new Scanner(System.in);
 			     
 			 do {
-				  Scanner scanner = new Scanner(System.in);
 				  System.out.print("Введите Данные :.............:" + "  ");
 		          String xx = scanner.nextLine();
 
