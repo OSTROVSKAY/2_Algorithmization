2536e438749a8046e9f009a4062ee1eff998cc35
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-05 10:46:29 +0300

    1

---
+++ src/Task_2_3/Task_2_3_res.java
@@ -0,0 +1,39 @@
+
+package Task_2_3;
+
+import java.util.Scanner;
+
+public class Task_2_3_res {
+	
+	// Ввод данных
+	
+	public int InputInt() {
+		  
+	      int check = 1;
+		  int x = 0;
+			     
+		  Scanner scanner = new Scanner(System.in);
+		  
+			 do {
+				  System.out.print("Введите Данные :.............: " + "  ");
+		          String xx = scanner.nextLine();
+
+		          check = 1;
+
+		            try {
+		          	      x = Integer.parseInt(xx);
+		                } 
+		                  catch (NumberFormatException e) {
+		                  
+		   	            check = 0;
+			                System.out.println("------------------------------------------");
+			                System.out.println("Данные введены не правильно. Введите снова");
+			                System.out.println("------------------------------------------");
+		                }
+		            
+		      System.out.println("----------------------------------");
+
+		    } while ( check == 0 );
+				     return x; 
+		 }
+}
