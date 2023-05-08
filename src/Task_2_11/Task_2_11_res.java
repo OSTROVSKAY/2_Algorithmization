a6e21d2f6570ce83790c0694f722549ac275a191
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-08 19:41:07 +0300

    1

--- src/Task_2_11/Task_2_11_res.java
+++ src/Task_2_11/Task_2_11_res.java
@@ -2,5 +2,69 @@
 package Task_2_11;
 
 public class Task_2_11_res {
-
+	
+	 // Заполнение двумерного массива
+	public void FillMass( int[][] array, int X, int Y) {
+		
+		 for (int i = 0; i < X; i++) {
+		        for (int j = 0; j < Y; j++) {
+		             array[i][j] = 0 + (int) ( Math.random()*(15-0) );
+		        }
+		    } 
+	}
+	
+	
+	// Вывод двумерного массива на печать
+	public void PrintMass( int[][] array ) {
+	
+		for (int i = 0; i < array.length; i++) {  // X - Число строк
+			  
+				for (int j = 0; j < array[i].length; j++) {  // Y - Число столбцов
+					
+					//System.out.print(array[i][j] + "\t");
+					
+					System.out.print(array[i][j] + "  ( " + i + " " + j + " )  " + "\t" );
+				}
+				System.out.println();
+		   	} 
+	}
+	
+	
+	 // Вывод на экран номера строк и строк матрицы, в которых число 5 встречается три и более раз.
+	public void PrintMass1( int[][] array ) {
+		
+		 int count = 0; // Число повторов числа 5
+         int count1 = 0;
+         
+         for (int i = 0; i < array.length; i++) {  // X - Число строк
+			  
+			  for (int j = 0; j < array[i].length; j++) {  // Y - Число столбцов
+					
+					if ( array[i][j] == 5) {
+						
+						count = count + 1;
+					}
+				}
+			  
+			         if ( count >= 3 ) { 
+			        	 
+			        	 count1 = count1 + 1;
+			        	 
+			        	  System.out.print("Номер строки : " + (i + 1) + "  ||  " + "Количество встреч числа 5 в строке : " + count + "  ||  " + "Значение элеметов в строке массива : " + "   ");
+			        	
+				       for (int j = 0; j < array[i].length; j++) {  // Y - Число столбцов
+					        System.out.print(array[i][j] + "\t");
+				            }
+				       System.out.println();
+			          }
+				count = 0;
+		   	}
+		  
+		    if ( count1 == 0 ) {     
+		    	 System.out.println("------------------------------------------------------------------");
+		    	 System.out.println("Строк матрицы, в которых число 5 встречается ТРИ и более раз - НЕТ");
+		    	 System.out.println("------------------------------------------------------------------");
+		    }
+	}
+	
 }
