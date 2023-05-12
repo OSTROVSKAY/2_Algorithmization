e5e0e63342cc2a855ac29858045a9baeb2372d4d
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-12 20:07:14 +0300

    1

--- src/Task_1_4/Task_1_4_res.java
+++ src/Task_1_4/Task_1_4_res.java
@@ -15,20 +15,20 @@ public class Task_1_4_res {
 	      Scanner scanner = new Scanner(System.in);
 	     
 	      do {
-		       System.out.print("Введите Данные :.............:" + "  ");
+		       System.out.print("Введите Данные :.............: " + "  ");
                String xx = scanner.nextLine();
 
                check = 1;
 
             try {
-          	     x = Integer.parseInt(xx);
+          	      x = Integer.parseInt(xx);
                 } 
                   catch (NumberFormatException e) {
                   
-   	            check = 0;
-	                System.out.println("------------------------------------------");
-	                System.out.println("Данные введены не правильно. Введите снова");
-	                System.out.println("------------------------------------------");
+   	             check = 0;
+	             System.out.println("------------------------------------------");
+	             System.out.println("Данные введены не правильно. Введите снова");
+	             System.out.println("------------------------------------------");
                 }
             
       System.out.println("----------------------------------");
