4a944e80caf0ddeb2da34906d49fcda242813730
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-05-17 08:57:57 +0300

    1

--- src/Task_1_1/Task_1_1_res.java
+++ src/Task_1_1/Task_1_1_res.java
@@ -1,37 +1,37 @@
+	
 package Task_1_1;
-
+	
 import java.util.Scanner;
-
+	
 public class Task_1_1_res {
 	
 	public int InputInt() {
-		  
-	      int check = 1;
-	      int x  = 0;
-	      
-	      Scanner scanner = new Scanner(System.in);
-	     
-	      do {
-		       System.out.print("Введите Данные :.............: " + "  ");
-               String xx = scanner.nextLine();
- 
-               check = 1;
-     
-                  try {
-                	    x = Integer.parseInt(xx);
-                      } 
-                        catch (NumberFormatException e) {
-                        
-         	            check = 0;
-     	                System.out.println("------------------------------------------");
-     	                System.out.println("Данные введены не правильно. Введите снова");
-     	                System.out.println("------------------------------------------");
-                      }
-                  
-            System.out.println("----------------------------------");
- 
-     } while ( check == 0 );
-		    return x; 
-  }
-
+	
+	int check = 1;
+	int x = 0;
+	
+	Scanner scanner = new Scanner(System.in);
+	
+	do {
+		System.out.print("Введите Данные :.............: " + "  ");
+		String xx = scanner.nextLine();
+		
+		check = 1;
+		
+		try {
+			x = Integer.parseInt(xx);
+			}
+			catch (NumberFormatException e) {
+					check = 0;
+					System.out.println("------------------------------------------");
+					System.out.println("Данные введены не правильно. Введите снова");
+					System.out.println("------------------------------------------");
+					}
+					
+				System.out.println("----------------------------------");
+				
+	} while ( check == 0 );
+			return x;
+	}
+	
 }
