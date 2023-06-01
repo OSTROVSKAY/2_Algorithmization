8cc509d6daedecf0d1783149d58bdb76c715e054
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-06-01 14:08:15 +0300

    1

---
+++ src/Task_4_12/Task_4_12_res.java
@@ -0,0 +1,58 @@
+	
+package Task_4_12;
+	
+import java.util.Scanner;
+	
+public class Task_4_12_res {
+	
+	// Ввод данных
+	public int InputInt() {
+		
+		int check = 1;
+		int x = 0;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		do {
+			System.out.print("Введите Данные :.............: " + "  ");
+			String xx = scanner.nextLine();
+			
+			check = 1;
+				
+				try {
+					x = Integer.parseInt(xx);
+					}
+					catch (NumberFormatException e) {
+						
+						check = 0;
+						System.out.println("------------------------------------------");
+						System.out.println("Данные введены не правильно. Введите снова");
+						System.out.println("------------------------------------------");
+						}
+						
+					System.out.println("-----------------------------------");
+					
+				} while ( check == 0 );
+				
+				return x;
+			}
+		
+		
+	// Вычисление суммы цифр в числе
+	public int Sum ( int number ) {
+		
+		int sum = 0;
+		
+		while ( number > 0 ) {
+		
+		int temp = number % 10;
+		
+		sum = sum + temp;
+		
+		number = ( number - temp ) / 10;
+		}
+		
+		return sum;
+	}
+	
+}
